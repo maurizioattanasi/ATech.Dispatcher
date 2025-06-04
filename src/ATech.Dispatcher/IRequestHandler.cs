119f5caef98e7cf3d18eb74bea742a9fc0791567
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2025-06-04 23:30:55 +0200

    added test project

--- src/ATech.Dispatcher/IRequestHandler.cs
+++ src/ATech.Dispatcher/IRequestHandler.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 namespace ATech.Dispatcher;
 
 public interface IRequestHandler<in TRequest>
---
+++ tests/ATech.Dispatcher.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ATech.Dispatcher.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
