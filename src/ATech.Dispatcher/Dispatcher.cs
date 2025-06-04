5d81b9752c307aef1d6dd8ee8fe61d650efd8960
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2025-06-04 22:53:08 +0200

    Dispatcher class completed

---
+++ src/ATech.Dispatcher/Dispatcher.cs
@@ -0,0 +1,38 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ATech.Dispatcher;
+
+public class Dispatcher(IServiceProvider serviceProvider) : IDispatcher
+{
+    public async Task SendAsync(IRequest request, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var requestType = request.GetType();
+        var handlerType = typeof(IRequestHandler<>).MakeGenericType(requestType);
+
+        var handler = serviceProvider.GetRequiredService(handlerType);
+        var method = handlerType.GetMethod(nameof(IRequestHandler<IRequest>.HandleAsync));
+
+        var result = method!.Invoke(handler, [request, cancellationToken]);
+
+        if (result is Task task)
+            await task.ConfigureAwait(false);
+    }
+
+    public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
+        where TResponse : notnull
+    {
+        ArgumentNullException.ThrowIfNull(request);
+
+        var requestType = request.GetType();
+        var handlerType = typeof(IRequestHandler<,>).MakeGenericType(requestType, typeof(TResponse));
+
+        var handler = serviceProvider.GetRequiredService(handlerType);
+        var method = handlerType.GetMethod(nameof(IRequestHandler<IRequest<TResponse>, TResponse>.HandleAsync));
+
+        var result = method!.Invoke(handler, [request, cancellationToken]);
+
+        return result is Task<TResponse> task ? await task.ConfigureAwait(false) : (TResponse)result!;
+    }
+}
--- src/ATech.Dispatcher/IDispatcher.cs
+++ src/ATech.Dispatcher/IDispatcher.cs
@@ -9,17 +9,3 @@ public interface IDispatcher
     Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
         where TResponse : notnull;
 }
-
-public class Dispatcher(IServiceProvider serviceProvider) : IDispatcher
-{
-    public Task SendAsync(IRequest request, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException("This method is not implemented. Use SendAsync<TResponse> instead.");
-    }
-
-    public Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default)
-        where TResponse : notnull
-    {
-        throw new NotImplementedException("This method is not implemented. Use SendAsync<TResponse> instead.");
-    }
-}
